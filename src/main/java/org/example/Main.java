0c44b40c5a26381a2197ac3823d2df5f7ebea519
Ctac <ctac.shamsheev@gmail.com>
2022-08-07 23:34:10 +0700

    mvn???

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,9 +20,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println( new Parser(new String[]{"-i", "-a", "out.txt", "in.txt"}).toString()); // sort-it.exe -i -a out.txt in.txt
-        System.out.println( new Parser(new String[]{"-s", "out.txt", "in1.txt", "in2.txt", "in3.txt"}).toString()); // sort-it.exe -s out.txt in1.txt in2.txt in3.txt
-        System.out.println( new Parser(new String[]{"-d", "-s", "out.txt", "in1.txt", "in2.txt"}).toString()); // sort-it.exe -d -s out.txt in1.txt in2.txt
+        System.out.println( new Parser(args));
+//        System.out.println( new Parser(new String[]{"-i", "-a", "out.txt", "in.txt"}).toString()); // sort-it.exe -i -a out.txt in.txt
+//        System.out.println( new Parser(new String[]{"-s", "out.txt", "in1.txt", "in2.txt", "in3.txt"}).toString()); // sort-it.exe -s out.txt in1.txt in2.txt in3.txt
+//        System.out.println( new Parser(new String[]{"-d", "-s", "out.txt", "in1.txt", "in2.txt"}).toString()); // sort-it.exe -d -s out.txt in1.txt in2.txt
         ArrayList<String> filenames = new ArrayList<>();
 
         for (int i = 1; i < 7; i++) { // TODO args list
